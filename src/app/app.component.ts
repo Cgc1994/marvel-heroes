97f3c6d7d10293dff16abb317b5062b13eb3186f
Carlos González <carlos.gonzalez@hispatec.com>
2024-11-26 21:18:50 +0100

    basic table model | interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import marvelData  from './assets/mock-data/wikipedia_marvel_data.json'; 
+import { HeroTableComponent } from '../app/components/hero-table/hero-table.component';
+import { Heroe } from '../app/models/heroe.model';
+
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeroTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  data: Heroe[] = marvelData;
+  columns: string[] = ['nameLabel', 'creatorLabel', 'genderLabel', 'citizenshipLabel', 'memberOfLabel', 'occupationLabel', 'skillsLabel'];
+
+  ngOnInit() {
+    console.log(this.data);
+  }
   
 }
