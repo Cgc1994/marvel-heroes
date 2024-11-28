0a54d05d9654547fd94cc2cb3cc726d3b371923a
Carlos González <carlos.gonzalez@hispatec.com>
2024-11-28 16:02:48 +0100

    chip component for table filtering

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,20 +1,44 @@
 import {LiveAnnouncer} from '@angular/cdk/a11y';
-import {AfterViewInit, Component, ViewChild, inject} from '@angular/core';
+import {AfterViewInit, Component, ViewChild, inject, ChangeDetectionStrategy, computed, model, signal} from '@angular/core';
 import {MatSort, Sort, MatSortModule} from '@angular/material/sort';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {FormsModule} from '@angular/forms';
+import {MatAutocompleteModule, MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import {MatChipInputEvent, MatChipsModule} from '@angular/material/chips';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+
 import marvelData  from '../../assets/mock-data/wikipedia_marvel_data.json'; 
 
 @Component({
   selector: 'app-table',
-  imports: [MatTableModule, MatSortModule],
+  imports: [MatTableModule, MatSortModule, MatFormFieldModule, MatChipsModule, MatIconModule, MatAutocompleteModule, FormsModule],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.css'
+  styleUrl: './table.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TableComponent {
-  private _liveAnnouncer = inject(LiveAnnouncer);
-
+  //table logic
   displayedColumns: string[] = ['nameLabel', 'creatorLabel', 'citizenshipLabel', 'genderLabel', 'memberOfLabel', 'occupationLabel', 'skillsLabel'];
   dataSource = new MatTableDataSource(marvelData);
+  private originalData = marvelData;
+
+  //sort
+  private _liveAnnouncer = inject(LiveAnnouncer);
+
+  //chips
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  readonly currentHeroe = model('');
+  readonly heroes = signal<string[]>([]);
+  readonly allHeroes = computed(() => marvelData.map(item => item.nameLabel));
+  readonly filteredHeroes = computed(() => {
+    const currentHeroe = this.currentHeroe().toLowerCase();
+    return currentHeroe
+      ? this.allHeroes().filter(heroe => heroe.toLowerCase().includes(currentHeroe))
+      : this.allHeroes();
+  });
+  readonly announcer = inject(LiveAnnouncer);
 
   @ViewChild(MatSort)
   sort: MatSort = new MatSort;
@@ -28,6 +52,47 @@ export class TableComponent {
       this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
     } else {
       this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    if (value) {
+      this.heroes.update(heroes => [...heroes, value]);
+    }
+    this.currentHeroe.set('');
+    this.filterTable();
+  }
+
+  remove(heroe: string): void {
+    this.heroes.update(heroes => {
+      const index = heroes.indexOf(heroe);
+      if (index < 0) {
+        return heroes;
+      }
+
+      heroes.splice(index, 1);
+      this.announcer.announce(`Removed ${heroe}`);
+      return [...heroes];
+    });
+    this.filterTable();
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {    
+    this.heroes.update(heroes => [...heroes, event.option.viewValue]);
+    this.currentHeroe.set('');
+    event.option.deselect();
+    this.filterTable();
+  }
+
+  private filterTable(): void {
+    const selectedHeroes = this.heroes();
+    if (selectedHeroes.length === 0) {
+      this.dataSource.data = this.originalData;
+    } else {
+      this.dataSource.data = this.originalData.filter(item =>
+        selectedHeroes.includes(item.nameLabel)
+      );
     }
   }
 
